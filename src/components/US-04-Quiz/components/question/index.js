9d4b5020259eb96638055dbc01f227df27cd2c81
SeacretCK <fraeulein_k@gmx.net>
2021-10-28 15:54:50 +0200

    radomized answers

--- src/components/US-04-Quiz/components/question/index.js
+++ src/components/US-04-Quiz/components/question/index.js
@@ -1,11 +1,13 @@
 function Question({question, correct_answer, incorrect_answers}) {
 
+  const answers = [correct_answer, ...incorrect_answers];
+  const shuffledAnswers = answers.sort((a, b) => 0.5 - Math.random());
+
   return(
 
       <div>
         <h5>{question}</h5>
-        <div>{correct_answer}</div>
-        {incorrect_answers.map((item, index) => (
+        {shuffledAnswers.map((item, index) => (
           <div key={index}>{item}</div>
         ))}
       </div>
