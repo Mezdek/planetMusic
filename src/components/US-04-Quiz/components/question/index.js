57e6d5a85a8e11c37d9482c332d8efd5e2185832
SeacretCK <fraeulein_k@gmx.net>
2021-10-28 22:33:21 +0200

    checking answer

--- src/components/US-04-Quiz/components/question/index.js
+++ src/components/US-04-Quiz/components/question/index.js
@@ -1,15 +1,32 @@
+import Button from "react-bootstrap/Button";
+import { useState, useEffect } from "react";
+
 function Question({question, correct_answer, incorrect_answers}) {
 
-  const answers = [correct_answer, ...incorrect_answers];
-  const shuffledAnswers = answers.sort((a, b) => 0.5 - Math.random());
+  const [answers, setAnswers] = useState([]);
+  const [clicked, setClicked] = useState(false);
+  const [correct, setCorrect] = useState(false);
+
+  useEffect(() => {
+    const answers = [correct_answer, ...incorrect_answers];
+    const shuffledAnswers = answers.sort((a, b) => 0.5 - Math.random());
+    setAnswers(shuffledAnswers);
+  }, [])
+
+  const checkAnswer = (e) => (e.target.innerText === correct_answer) ? setCorrect(true) : setCorrect(false);
 
   return(
 
       <div>
-        <h5>{question}</h5>
-        {shuffledAnswers.map((item, index) => (
-          <div key={index}>{item}</div>
-        ))}
+        <h3>{question}</h3>
+        <div className="d-grid gap-2">
+        {answers.map((item, index) => (
+          <Button variant="info" size="lg" key={index} onClick={(e) => {checkAnswer(e); setClicked(true)}}>
+            {item}
+          </Button>))}
+        </div>
+        {clicked ? (correct) ? <h2>Correct!</h2> : <h2>Wrong :/</h2> : null}
+
       </div>
   )
 }
--- src/components/US-04-Quiz/index.js
+++ src/components/US-04-Quiz/index.js
@@ -2,12 +2,13 @@ import Container from 'react-bootstrap/Container';
 import StartPage from './components/StartPage';
 import Questions from './components/questions';
 import { useState } from 'react';
+import "./quiz.css";
 
 function Quiz() {
   const [quizStart, setQuizStart] = useState(false);
 
   return (
-    <Container fluid='md'>
+    <Container fluid='md' className="quiz-main-container">
       {quizStart ? (
         <Questions />
       ) : (
