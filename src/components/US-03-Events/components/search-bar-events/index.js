1f5d982377c53cb1089a626aa08b2a0fd4d4c8fb
Marta <martagaworek05@gmail.com>
2021-11-07 19:39:04 +0100

    Added parameters to API url, date ascending, location, artist. Created dynamic  inputs for search categories

--- src/components/US-03-Events/components/search-bar-events/index.js
+++ src/components/US-03-Events/components/search-bar-events/index.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import './style.css'
 
-export default function SearchBarEvents() {
+export default function SearchBarEvents(props) {
+    const {searchCategories, selectedSearchCategory, setSelectedSearchCategory} = props;
+
+// if I search by location, take search Value, apply it to url as location search params and when the search button is true fetch the data
 
     return (
         <div className="searchBarContainer">
-           <label for="searchBar">SEARCH BY:</label>
-           <select name="eventSearchSelection" id="eventSearchSelection">
-                <option value="location">Location</option>
-                <option value="date">Date</option>
-                <option value="artist">Artist</option>
+           <label for="searchByLabel" className="searchByLabel">SEARCH BY:</label>
+           <select name="eventSearchSelection" id="eventSearchSelection" onChange={(event)=> setSelectedSearchCategory(event.target.value)}>
+               <option value={searchCategories[0]} > SELECT </option>
+                <option value={searchCategories[1]} >Location</option>
+                <option value={searchCategories[2]} >Date</option>
+                <option value={searchCategories[3]} >Artist</option>
+                {/* <option value="date">Date</option>
+                <option value="artist">Artist</option> */}
             </select>
-            
-            <input></input>
-            <button>Search</button>
+
+            {(selectedSearchCategory == searchCategories[0]) ? "" : ""}
+            {(selectedSearchCategory == searchCategories[1]) ? 
+            <input placeholder="search by city" className="inputSearchCategory" ></input>: ""}
+            {(selectedSearchCategory == searchCategories[2]) ? 
+            (<><input placeholder="Start Date yyyy/mm/dd" className="inputSearchCategory">
+                </input> <input placeholder="End Date yyyy/mm/dd"></input> </>) : ""}
+            {(selectedSearchCategory == searchCategories[3]) ? 
+            <input placeholder="search by artist" className="inputSearchCategory"></input>: ""}
+           
+            <button className="searchByCategoryButton">Search</button>
         </div>
     )
 }
--- src/components/US-03-Events/index.js
+++ src/components/US-03-Events/index.js
@@ -6,17 +6,48 @@ import Container from 'react-bootstrap/Container';
 import SearchBarEvents from './components/search-bar-events';
 
 function FinalEvent() {
-    const [events, setEvents] = React.useState([])
-    const [visible, setVisible] = React.useState(6)
+  
+  const [events, setEvents] = React.useState([])
+  const [visible, setVisible] = React.useState(6)
 
-    
+    const searchCategories = [
+      "",
+      "Location",
+      "Date", 
+      "Artist"
+    ] 
+  
+  const [selectedSearchCategory, setSelectedSearchCategory] = React.useState(searchCategories[0])
+
+
+  const [selectedCity, setSelectedCity] = React.useState('')
+  const [selectedStartDate, setSelectedStartDate] = React.useState('providetodaydate')
+  const [selectedEndDate, setSelectedEndDate] = React.useState('providetodaydate')
+  const [selectedArtist, setSelectedArtist] = React.useState('providetodaydate')
+
     React.useEffect(
       ()=>{
-        fetch("https://app.ticketmaster.com/discovery/v2/events.json?countryCode=DE&apikey=3HsiJ0SglnkQTD4lRtIkQqVgI5i9kB8z&size=200&classificationName=Music")
+        const url = new URL("https://app.ticketmaster.com/discovery/v2/events.json")
+        url.searchParams.append("countryCode", "DE")
+        url.searchParams.append("apikey", "3HsiJ0SglnkQTD4lRtIkQqVgI5i9kB8z")
+        url.searchParams.append("size", "200")
+        url.searchParams.append("sort", "date,asc")
+
+        //if selectedCity string is not empty(which evaluates to true), return all the events with applied filter
+        if(selectedCity) {
+          url.searchParams.append("city", selectedCity)
+        }
+
+        // url.searchParams.append("classificationName", "Music")
+        // url.searchParams.append("startDateTime", selectedStartDate)
+        // url.searchParams.append("endDateTime", selectedEndDate)
+        // url.searchParams.append("", selectedArtist)
+        // startDateTime=${}&endDateTime=${}&city=${location}
+
+        fetch(url.href)
         .then((response)=>{return response.json()})
         .then((response)=>{
           setEvents(response._embedded.events)
-          console.log(response._embedded.events)
         })
         .catch((err) => {
           console.log('Not able to fetch API data', err)
@@ -29,7 +60,11 @@ function FinalEvent() {
         <Container>
         <div className="allElementsContainer">
           <h1 className="pageIntro">MUSIC EVENTS IN GERMANY:</h1>
-          <SearchBarEvents/>
+          <SearchBarEvents 
+          searchCategories={searchCategories}
+          selectedSearchCategory={selectedSearchCategory} 
+          setSelectedSearchCategory={setSelectedSearchCategory}
+          />
           <div className ="allEventsContainer">
           {
           events.slice(0, visible).map((event, index)=>{
