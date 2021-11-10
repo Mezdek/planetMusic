2eadf8b859ff8f341a115c61b1d8af50a63ee793
SeacretCK <fraeulein_k@gmx.net>
2021-11-10 15:40:13 +0100

    added countdown for question

--- src/components/US-04-Quiz/components/Question/index.js
+++ src/components/US-04-Quiz/components/Question/index.js
@@ -14,32 +14,38 @@ function Question({
 }) {
   const [answers, setAnswers] = useState([]);
   const [clicked, setClicked] = useState(false);
+  const [countdown, setCountdown] = useState(30);
+  const [outOfTime, setOutOfTime] = useState(false);
 
   useEffect(() => {
+    // combines the correct answer with the array of wrong answers and shuffle them
     const answers = [correct_answer, ...incorrect_answers];
     const shuffledAnswers = answers.sort((a, b) => 0.5 - Math.random());
     setAnswers(shuffledAnswers);
   }, []);
-  // combines the correct answer with the array of wrong answers and shuffle them
+
+  useEffect(() => {
+    // countdown starts onMount and continues until clicked=true
+    countdown > 0 &&
+      !clicked &&
+      setTimeout(() => setCountdown(countdown - 1), 1000);
+    countdown === 0 && setOutOfTime(true);
+  }, [countdown]);
 
   const checkAnswer = (e) => {
     const difficultyScores = { easy: 10, medium: 30, hard: 50 };
     if (e.target.innerText === correct_answer) {
       e.target.classList.add('btn-success');
       e.target.classList.remove('btn-outline-info');
-      //e.target.className = 'btn btn-lg btn-success';
       setScore(score + difficultyScores[difficulty]);
     } else {
       // show the clicked answer red
       e.target.classList.add('btn-danger');
       e.target.classList.remove('btn-outline-info');
-      //e.target.className = 'btn btn-lg btn-danger';
-      // search for the right answer and display green
       e.target.parentNode.childNodes.forEach((answer) => {
         if (answer.innerText === correct_answer) {
           answer.classList.add('btn-success');
           answer.classList.remove('btn-outline-info');
-          //answer.className = 'btn btn-lg btn-success';
         }
       });
     }
@@ -52,14 +58,18 @@ function Question({
         <p>Question {questionCounter + 1}/10</p>
       </div>
       <h2 className='text-center mb-5'>Score: {score}</h2>
+      <h1 className='text-center mb-5'>
+        {outOfTime ? 'Time is up!' : `${countdown} seconds left`}
+      </h1>
       <h3 className='text-center'>{question}</h3>
       <div className='d-grid gap-2 mt-5'>
         {answers.map((item, index) => (
           <Button
-            className=''
-            variant='outline-info'
+            variant={
+              item === correct_answer && outOfTime ? 'success' : 'outline-info'
+            }
             size='lg'
-            disabled={clicked}
+            disabled={clicked || outOfTime}
             key={`answers-${index}`}
             onClick={(e) => {
               checkAnswer(e);
@@ -74,7 +84,7 @@ function Question({
         <Button
           variant='success'
           size='lg'
-          disabled={!clicked}
+          disabled={!clicked && !outOfTime}
           onClick={nextQuestion}
         >
           {questionCounter === questionsQuantity - 1 ? 'Finish!' : 'Next'}
